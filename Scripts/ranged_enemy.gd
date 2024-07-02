4160803f8c1c1dd448bd1ed4a8db31df8564bbe1
José Ricardo <ricardoanimation2@gmail.com>
2024-07-02 00:34:24 -0300

    start ranged enemy scene and script

---
+++ Scripts/ranged_enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+const SPEED = 30
+var player = null
+var player_is_near := false
+var player_is_colliding := false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
