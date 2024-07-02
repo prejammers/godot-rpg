7fcc4ca82dd10cbaeae78a02a21edc6285e5366e
José Ricardo <ricardoanimation2@gmail.com>
2024-07-02 01:36:43 -0300

    add player detection and collision with enemy

--- Scripts/ranged_enemy.gd
+++ Scripts/ranged_enemy.gd
@@ -4,6 +4,7 @@ const SPEED = 30
 var player = null
 var player_is_near := false
 var player_is_colliding := false
+var hit_cooldown := false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,30 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if player_is_colliding == true and hit_cooldown == false:
+		player.cur_hp = player.cur_hp - 1
+		hit_cooldown = true
+		start_timer()
+
+
+func start_timer():
+	while true:
+		await get_tree().create_timer(1.0).timeout
+		hit_cooldown = false
+
+
+func _on_player_detection_body_entered(body):
+	player = body
+	player_is_near = true
+
+func _on_player_detection_body_exited(body):
+	player_is_near = false
+
+
+func _on_hurtbox_body_entered(body):
+	player = body
+	player_is_colliding = true
+
+func _on_hurtbox_body_exited(body):
+	player_is_colliding = false
