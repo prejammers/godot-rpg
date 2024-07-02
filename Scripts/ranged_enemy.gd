78e60220928e3668c87db0acf72f57e58adb8982
José Ricardo <ricardoanimation2@gmail.com>
2024-07-02 11:33:19 -0300

    create arrow scene and enemy's shoot function

--- Scripts/ranged_enemy.gd
+++ Scripts/ranged_enemy.gd
@@ -6,6 +6,8 @@ var player_is_near := false
 var player_is_colliding := false
 var hit_cooldown := false
 
+@onready var projectile_scene = load("res://Scenes/arrow.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +19,10 @@ func _physics_process(delta):
 		player.cur_hp = player.cur_hp - 1
 		hit_cooldown = true
 		set_hit_cooldown_timer()
+	
+	if $Timer.time_left == 0 and player_is_near:
+		shoot_projectile()
+		
 
 
 func set_hit_cooldown_timer():
@@ -25,12 +31,23 @@ func set_hit_cooldown_timer():
 		hit_cooldown = false
 
 
+func shoot_projectile():
+	$AimToPlayer.look_at(player.global_position)
+	var projectile : RigidBody2D = projectile_scene.instance()
+	projectile.global_position = self.global_position
+	projectile.rotation_degrees = $AimToPlayer.rotation_degrees
+	projectile.apply_impulse(Vector2(500, 0).rotated(projectile.rotation))
+	get_tree().current_scene.add_child(projectile)
+
+
 func _on_player_detection_body_entered(body):
 	player = body
 	player_is_near = true
+	$Timer.start()
 
 func _on_player_detection_body_exited(body):
 	player_is_near = false
+	$Timer.stop()
 
 
 func _on_hurtbox_body_entered(body):
