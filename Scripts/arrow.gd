279463f39db6680f5f611009ea8d50bfe80a49ae
José Ricardo <ricardoanimation2@gmail.com>
2024-07-09 20:42:58 -0300

    replace old arrow scene and script with test one

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -1,16 +1,15 @@
 extends Area2D
 
-const SPEED = 300
+var speed = 300
 
 func _ready():
 	set_as_top_level(true)
-
+	
 func _process(delta):
-	position += Vector2.RIGHT.rotated(rotation) * SPEED * delta
-
+	position += Vector2.RIGHT.rotated(rotation) * speed * delta
+	
 func arrow():
 	pass
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
+func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
-
