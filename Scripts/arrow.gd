7075416db9b209de076e720982d88b118f0521a9
José Ricardo <ricardoanimation2@gmail.com>
2024-07-12 10:54:13 -0300

    fix arrow scene and script

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 var speed = 300
 
-
+func _ready():
+	set_as_top_level(true)
+	
 func _process(delta):
-	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * speed * delta
+	position += Vector2.RIGHT.rotated(rotation) * speed * delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
