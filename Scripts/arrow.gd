3d2abe97b8393f722c8afa3d3cac0b8cca569862
José Ricardo <ricardoanimation2@gmail.com>
2024-07-12 10:51:51 -0300

    revert arrow variable change

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-const SPEED = 300
+var speed = 300
 
 
 func _process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * SPEED * delta
+	position += direction * speed * delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
