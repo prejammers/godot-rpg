596de09fd4fe068960e9b8bd78ff288319a3faa1
CodeBr1dge <dogemanxd2000@gmail.com>
2024-07-29 19:53:07 -0600

    When player dies, world scene resets (though player Death animation not working), readjusted slime enemy health bar to reflect actual hp
    So the player dies and I want a death animation to play, but instead their idle animations play instead of the death animation as they're always checking if the player is not moving.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,10 +7,11 @@ var max_hp = 10
 var cur_hp = 10
 var player_alive = true
 @onready var healthBar = $PlayerHealthBar
+@onready var death_sound = $Explosion 
 const FireBall = preload("res://Scenes/Fireball.tscn")
 const IceWall = preload("res://Scenes/IceWall.tscn")
 var current_dir = "none"
-const SPEED = 100
+var SPEED = 100
 
 var experience = 0
 var experience_level = 1
@@ -20,15 +21,28 @@ var collected_experience = 0
 @onready var expBar = get_node("GUILayer/GUI/ExperienceBar") 
 @onready var lblLevel = get_node("GUILayer/GUI/ExperienceBar/lbl_level")
 
-var shoot_cooldown: bool = false
+#var shoot_cooldown: bool = false
+var timer = null
+var shoot_delay = .5
+var shoot_cooldown = true
+
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit() #exits the game when pressing escape
 
 func _ready():
+	#create timer for shoot delay
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(shoot_delay)
+	timer.connect("timeout", Callable(self, "on_timeout_complete"))
+	add_child(timer)
 	$PlayerAnimation.play("idle_down")
 	set_expbar(experience, calculate_experiencecap())
-	
+
+#on timer's timeout complete
+func on_timeout_complete():
+	shoot_cooldown = true
 func _process(delta):
 	pass
 	
@@ -37,43 +51,70 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide()
 	enemy_attack()
-	
-	if cur_hp <= 0:
-		player_alive = false #add whatever death needs to be added (respawn, back to menu, etc.)
-		cur_hp = 0
-		print("Player died")
+	#
+	#if cur_hp <= 0:
+		##player_alive = false #add whatever death needs to be added (respawn, back to menu, etc.)
+		#cur_hp = 0
+		##var shoot_cooldown = true
+	if cur_hp <= 0 and player_alive:
+		death()
+func death():
+	SPEED = 0
+	player_alive = false
+	death_sound.play()
+	#$PlayerAnimation.play("death_animation")
+	play_anim(0)
+	await get_tree().create_timer(3).timeout
+	get_tree().reload_current_scene()
+	print("Player died")
 	
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * SPEED
 
-func start_timer():
-	while true:
-		await get_tree().create_timer(1.0).timeout
-		shoot_cooldown = false
+#func start_timer():
+	#while true:
+		#await get_tree().create_timer(1.0).timeout
+		#shoot_cooldown = false
 		
-func Shoot():
-	if not shoot_cooldown:
-		shoot_cooldown = true
+#func Shoot():
+	#if not shoot_cooldown:
+		#shoot_cooldown = true
+		#var fireball = FireBall.instantiate()
+		#get_parent().add_child(fireball)
+		#fireball.global_transform = $Aim.global_transform
+		##start_timer()
+		#
+#func ice_Wall():
+	#if not shoot_cooldown:
+		#shoot_cooldown = true
+		#var icewall = IceWall.instantiate()
+		#get_parent().add_child(icewall)
+		#icewall.position = $Aim.get_global_mouse_position()
+		#start_timer()
+		
+func player_movement(delta):
+	if Input.is_action_pressed("fire") and shoot_cooldown:
+		#Shoot()
 		var fireball = FireBall.instantiate()
 		get_parent().add_child(fireball)
 		fireball.global_transform = $Aim.global_transform
-		start_timer()
+		# Disable shooting until timer's cooldown is complete
+		shoot_cooldown = false
+		#start timer
+		timer.start()
+		#$attack_cooldown/label_value.set_text(str(timer.get_time_left()))
 		
-func ice_Wall():
-	if not shoot_cooldown:
-		shoot_cooldown = true
+	if Input.is_action_pressed("IceWall") and shoot_cooldown:
+		#var IceWall = IceWall.instance()
+		#ice_Wall()
+		# Disable shooting until timer's cooldown is complete
+		shoot_cooldown = false
 		var icewall = IceWall.instantiate()
 		get_parent().add_child(icewall)
 		icewall.position = $Aim.get_global_mouse_position()
-		start_timer()
-		
-func player_movement(delta):
-	if Input.is_action_pressed("fire") and shoot_cooldown == false:
-		Shoot()
-		
-	if Input.is_action_pressed("IceWall") and shoot_cooldown == false:
-		ice_Wall()
+		#start timer
+		timer.start()
 		
 	if Input.is_action_pressed("alt_fire"):
 		pass
@@ -118,7 +159,6 @@ func player_movement(delta):
 func play_anim(movement):
 	var dir = current_dir
 	var anim = $"Character Sprite"
-	
 	if dir == "right":
 		if movement == 1:
 			$PlayerAnimation.play("walk_right")
@@ -146,6 +186,9 @@ func play_anim(movement):
 		elif movement == 0:
 			$PlayerAnimation.play("idle_down")
 			#anim.play("idle_down")
+	elif player_alive == false and movement == 0:
+		$PlayerAnimation.play("death_animation")
+		$ParticlesMove.emitting = false
 
 func player():
 	pass
@@ -235,3 +278,6 @@ func calculate_experiencecap():
 func set_expbar(set_value = 1, set_max_value = 100):
 	expBar.value = set_value
 	expBar.max_value = set_max_value
+func levelup():
+	if experience_level == 2:
+		shoot_delay - .2
